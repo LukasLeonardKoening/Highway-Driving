22278ad3e28503b4dd5cc640a334ca0ef8bd7030
Lukas Leonard Köning <llkoening@appuality.de>
2020-02-11 17:53:23 +0100

    Created trajactory files

---
+++ src/trajectory_gen.cpp
@@ -0,0 +1,7 @@
+//
+//  trajectory_gen.cpp
+//  Highway-Driving
+//
+//  Created by Lukas Leonard Köning on 07.02.20.
+//  Copyright © 2020 Lukas Leonard Köning. All rights reserved.
+//
---
+++ src/trajectory_gen.hpp
@@ -0,0 +1,7 @@
+//
+//  trajectory_gen.hpp
+//  Highway-Driving
+//
+//  Created by Lukas Leonard Köning on 07.02.20.
+//  Copyright © 2020 Lukas Leonard Köning. All rights reserved.
+//
