ac397b8268ed110bd506edd47600d535f1a84f03
Lukas Leonard Köning <llkoening@appuality.de>
2020-02-11 17:54:54 +0100

    Added framework

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -5,3 +5,59 @@
 //  Created by Lukas Leonard Köning on 07.02.20.
 //  Copyright © 2020 Lukas Leonard Köning. All rights reserved.
 //
+
+#include "vehicle.hpp"
+#include <string>
+#include <vector>
+#include "trajectory_gen.hpp"
+
+using std::string;
+using std::vector;
+using STATE = Vehicle::STATE;
+
+Vehicle::Vehicle(){}
+Vehicle::~Vehicle() {}
+
+Vehicle::Vehicle(int lane, float x, float y, float s, float d, float speed, float yaw, STATE state) {
+    this->lane = lane;
+    this->x = x;
+    this->y = y;
+    this->s = s;
+    this->d = d;
+    this->speed = speed;
+    this->yaw = yaw;
+    this->state = state;
+}
+
+vector<Vehicle> Vehicle::select_successor_state(map<int, vector<Vehicle>> &predictions, vector<double> previous_x, vector<double> previous_y, double previous_s, double previous_d, vector<vector<double>> map) {
+    Trajectory_Generator gen = Trajectory_Generator(map[0], map[1], map[2]);
+    return gen.generate_keep_lane(*this, previous_x, previous_y, previous_s, previous_d);
+}
+
+vector<STATE> Vehicle::get_possible_next_states(STATE &current_state) {
+    vector<STATE> states;
+    states.push_back(KEEP_LANE);
+    switch (current_state) {
+        case KEEP_LANE:
+            states.push_back(PREP_LANE_CHANGE_LEFT);
+            states.push_back(PREP_LANE_CHANGE_RIGHT);
+            break;
+        case PREP_LANE_CHANGE_RIGHT:
+            if (this->lane != 0) {
+                states.push_back(PREP_LANE_CHANGE_RIGHT);
+                states.push_back(CHANGE_LANE_RIGHT);
+            }
+            break;
+        case PREP_LANE_CHANGE_LEFT:
+            if (this->lane != num_lanes-1) {
+                states.push_back(PREP_LANE_CHANGE_LEFT);
+                states.push_back(CHANGE_LANE_LEFT);
+            }
+            break;
+        case CHANGE_LANE_LEFT:
+            break;
+        case CHANGE_LANE_RIGHT:
+            break;
+    }
+    return states;
+}
--- src/vehicle.hpp
+++ src/vehicle.hpp
@@ -5,3 +5,59 @@
 //  Created by Lukas Leonard Köning on 07.02.20.
 //  Copyright © 2020 Lukas Leonard Köning. All rights reserved.
 //
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <map>
+
+using std::string;
+using std::vector;
+using std::map;
+
+class Vehicle {
+public:
+    enum STATE {
+        KEEP_LANE, PREP_LANE_CHANGE_LEFT, PREP_LANE_CHANGE_RIGHT, CHANGE_LANE_LEFT, CHANGE_LANE_RIGHT
+    };
+    
+    // Constructors
+    Vehicle();
+    Vehicle(int lane, float x, float y, float s, float d, float speed, float yaw, STATE state=KEEP_LANE);
+
+    //Deconstructor
+    ~Vehicle();
+    
+    //Functions
+    vector<Vehicle> select_successor_state(map<int, vector<Vehicle>> &predictions,
+                                           vector<double> previous_x,
+                                           vector<double> previous_y,
+                                           double previous_s,
+                                           double previous_d,
+                                           vector<vector<double>> map);
+    vector<STATE> get_possible_next_states(STATE &current_state);
+    
+    // static variables
+    int num_lanes = 3;
+    
+    // vehicle variables
+    int lane;
+    float x;
+    float y;
+    float s;
+    float d;
+    float speed;
+    float yaw;
+    STATE state;
+    
+    
+private:
+    //private variables
+    
+};
+
+
+#endif /* VEHICLE_H */
